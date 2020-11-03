931f156a53bf4a87e5cbab9a1568aca643c3fcbe
KBarr <ksamuels6@yahoo.com>
2020-11-02 18:38:59 -0600

    Assignment 4 part 2 Controllers 1

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,22 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.xml.stream.Location;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotNull
+    private Location location;
 
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Employer() {}
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,23 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+import javax.xml.stream.Location;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 200, message = "Name must be between 3 and 200 characters")
+    private Skill skill;
+
+    public Skill getSkill() {
+        return skill;
+    }
+
+    public void setSkill(Skill skill) {
+        this.skill = skill;
+    }
+
+    public Skill() {
+    }
 }
